99a968ecf7173ab051ce4943ed53718eb436135a
AugustasCepas <augustas.cepas@gmail.com>
2022-01-23 14:55:26 +0200

    App update to work with parquet instead of csv

--- README.md
+++ README.md
@@ -4,38 +4,40 @@
 
 ## To install & run:
 ``` 
+Place database.csv file into the /src folder
+
+Commands for the terminal:
 1) cd geolocation_task/src
-2) go build main.go
-3) py geolocation_test.py --executable main.exe --database database.csv
+2) go run main.go (this creates database.parquet file)
+3) go build main.go
+4) py geolocation_test.py --executable main.exe --database database.parquet
 
 ```
 
-## Things to mention:
-```
-1) Before launching, database.csv file must be placed to the /src folder
-
-```
-
-## Other useful commands:
-
-# Python:
-py geolocation_test.py --executable sample_app.exe --database database.csv
-py geolocation_test.py --executable main.exe --database database.csv
-
-# CPP
-g++ sample_app.cpp -o sample_app
-./sample_app database.csv
 
 # Performance figures
 
-Database loaded Memory usage: 722.71mb Load time: 1s
-OK    1.0.0.0 US Los Angeles Memory usage: 722.72mb Lookup time: 0ns
-OK    71.6.28.0 US San Jose Memory usage: 722.72mb Lookup time: 3ms
-OK    71.6.28.255 US San Jose Memory usage: 722.73mb Lookup time: 2ms
-OK    71.6.29.0 US Concord Memory usage: 722.73mb Lookup time: 2ms
-OK    53.103.144.0 DE Stuttgart Memory usage: 722.77mb Lookup time: 2ms
-OK    53.255.255.255 DE Stuttgart Memory usage: 722.77mb Lookup time: 1ms
-OK    54.0.0.0 US Rahway Memory usage: 722.79mb Lookup time: 999μs
-OK    223.255.255.255 AU Brisbane Memory usage: 722.8mb Lookup time: 10ms
-OK    5.44.16.0 GB Hastings Memory usage: 722.8mb Lookup time: 1ms
-OK    8.24.99.0 US Hastings Memory usage: 722.8mb Lookup time: 0ns
+Database loaded Memory usage: 1.62gb Load time: 1s
+OK    1.0.0.0 US Los Angeles Memory usage: 1.62gb Lookup time: 760μs
+OK    71.6.28.0 US San Jose Memory usage: 1.62gb Lookup time: 0ns
+OK    71.6.28.255 US San Jose Memory usage: 1.62gb Lookup time: 166μs
+OK    71.6.29.0 US Concord Memory usage: 1.62gb Lookup time: 0ns
+OK    53.103.144.0 DE Stuttgart Memory usage: 1.62gb Lookup time: 0ns
+OK    53.255.255.255 DE Stuttgart Memory usage: 1.62gb Lookup time: 0ns
+OK    54.0.0.0 US Rahway Memory usage: 1.62gb Lookup time: 0ns
+OK    223.255.255.255 AU Brisbane Memory usage: 1.62gb Lookup time: 508μs
+OK    5.44.16.0 GB Hastings Memory usage: 1.62gb Lookup time: 0ns
+OK    8.24.99.0 US Hastings Memory usage: 1.62gb Lookup time: 0ns
+
+Load time points: 1161 pts
+Memory usage points: 16609 pts
+Lookup time points: 1436 pts
+Total points: 19206 pts
+
+# Things to mention
+
+Praquet file takes less space and should take less time to load (2-3x less) https://medium.com/@u.praneel.nihar/improving-read-write-store-performance-by-changing-file-formats-serialization-protocols-bfdb13114004
+
+However, it needs a header and to read it you need a structure, which needs a lot of memory.
+
+I have tried reading a line and appending start, end, code and city splices, this way it takes less memory, but takes 60seconds to read a db.
--- src/main.go
+++ src/main.go
@@ -12,68 +12,86 @@ import (
 	"strconv"
 	"strings"
 	"unicode/utf8"
+
+	"github.com/xitongsys/parquet-go-source/local"
+	"github.com/xitongsys/parquet-go/parquet"
+	"github.com/xitongsys/parquet-go/reader"
+	"github.com/xitongsys/parquet-go/writer"
 )
 
-var finishValues []int
-var startValues []int
-var countryTags []string
-var cities []string
+type GeolocationData struct {
+	Start int    `parquet:"name=start, type=INT64"`
+	End   int    `parquet:"name=end, type=INT64"`
+	Code  string `parquet:"name=code, type=BYTE_ARRAY, convertedtype=UTF8"`
+	City  string `parquet:"name=city, type=BYTE_ARRAY, convertedtype=UTF8"`
+}
+
+var geolocations []GeolocationData
+
+var convertFileName string = "database"
+var loadFileName string
 
 func main() {
+	argsPassed := os.Args[1:]
+
+	if len(argsPassed) != 1 {
+		csvToParquet()
+		os.Exit(1)
+	}
+	loadFileName = argsPassed[0]
+
 	fmt.Println("READY")
 	readInput()
+}
+
+func readParquet() {
+	fr, err := local.NewLocalFileReader(loadFileName)
+	if err != nil {
+		log.Println("Can't open file")
+		return
+	}
+
+	pr, err := reader.NewParquetReader(fr, new(GeolocationData), 4)
+	if err != nil {
+		log.Println("Can't create parquet reader", err)
+		return
+	}
+
+	rows := int(pr.GetNumRows())
+	geolocations = make([]GeolocationData, rows)
+
+	if err = pr.Read(&geolocations); err != nil {
+		log.Println("Read error", err)
+	}
+
+	pr.ReadStop()
+	fr.Close()
 }
 
 func findGeoLocation(ipSum int) int {
 	r := -1 // not found
 	start := 0
-	end := len(finishValues) - 1
+	end := len(geolocations) - 1
 	for start <= end {
 		mid := (start + end) / 2
-		if startValues[mid] <= ipSum && ipSum <= finishValues[mid] {
-			r = mid // found
-			fmt.Println(countryTags[r] + "," + cities[r])
-			break
-		} else if finishValues[mid] < ipSum {
+		if geolocations[mid].Start <= ipSum {
+			if ipSum <= geolocations[mid].End {
+				r = mid // found
+				fmt.Println(geolocations[r].Code + "," + geolocations[r].City)
+				break
+			}
+		}
+
+		if geolocations[mid].End < ipSum {
 			start = mid + 1
-		} else if finishValues[mid] > ipSum {
+		} else {
 			end = mid - 1
 		}
 	}
 	return r
 }
 
-func readCSV() {
-	csvIn, err := os.Open("./database.csv")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	r := csv.NewReader(csvIn)
-
-	for {
-		rec, err := r.Read()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatal(err)
-		}
-
-		start, _ := strconv.Atoi(rec[0])
-		finish, _ := strconv.Atoi(rec[1])
-		countryTag := rec[2]
-		city := rec[5]
-
-		startValues = append(startValues, start)
-		finishValues = append(finishValues, finish)
-		countryTags = append(countryTags, countryTag)
-		cities = append(cities, city)
-	}
-}
-
 func calculateIPSum(input []int) int {
-
 	result := 0
 	for i := 0; i < 4; i++ {
 		power := 3 - i
@@ -127,16 +145,17 @@ func readInput() {
 	} else if inputLength == 2 {
 		command = inputStrings[0]
 	} else {
-		exit()
+		os.Exit(1)
 	}
 
 	ipSum := 0
 	if command == "LOAD" {
-		readCSV()
+		readParquet()
 		fmt.Println("OK")
 
 	} else if command == "EXIT" {
-		exit()
+		fmt.Println("OK")
+		os.Exit(0)
 
 	} else if command == "LOOKUP" {
 		ip := trimLastChar(inputStrings[1])
@@ -148,13 +167,63 @@ func readInput() {
 			ipSum = calculateIPSum(inputSlice)
 		}
 
-		findGeoLocation(ipSum)
+		go findGeoLocation(ipSum)
 	}
 
 	readInput()
 }
 
-func exit() {
-	fmt.Println("OK")
-	os.Exit(0)
+func csvToParquet() {
+	var err error
+	var start int = 0
+	var end int = 0
+
+	fw, err := local.NewLocalFileWriter(convertFileName + ".parquet")
+	if err != nil {
+		log.Println("Can't create local file", err)
+		return
+	}
+
+	pw, err := writer.NewParquetWriter(fw, new(GeolocationData), 2)
+	if err != nil {
+		log.Println("Can't create parquet writer", err)
+		return
+	}
+
+	pw.RowGroupSize = 128 * 1024 * 1024 //128M
+	pw.CompressionType = parquet.CompressionCodec_SNAPPY
+
+	csvFile, _ := os.Open(convertFileName + ".csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+
+		start, _ = strconv.Atoi(line[0])
+		end, _ = strconv.Atoi(line[1])
+
+		shoe := GeolocationData{
+			Start: start,
+			End:   end,
+			Code:  line[2],
+			City:  line[5],
+		}
+
+		if err = pw.Write(shoe); err != nil {
+			log.Println("Write error", err)
+		}
+	}
+
+	if err = pw.WriteStop(); err != nil {
+		log.Println("WriteStop error", err)
+		return
+	}
+
+	log.Println("Write Finished")
+	fw.Close()
 }
